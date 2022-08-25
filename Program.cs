1c21a3ffcc560a85bde90c89b500ac118860f1ce
Mayara Pinheiro <mayarapinheirodesousa@hotmail.com>
2022-08-25 19:04:23 -0300

    Versão 1.0

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PulaSapo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var entrada= Console.ReadLine().Split(" ");
+            var alturaDoPulo= int.Parse(entrada[0]);
+            var numeroDeCanos= int.Parse(entrada[1]);
+
+            string[] pulo = Console.ReadLine().Split(' ');
+            int[] pulos = Array.ConvertAll(pulo, int.Parse);
+
+            bool controlador = true;
+
+            for (int i = 0; i < (numeroDeCanos - 1); i++) {
+               if ((pulos[i] - pulos[i + 1]) < (-alturaDoPulo))
+                 controlador = false;
+              else if ((pulos[i] - pulos[i + 1]) > alturaDoPulo)
+                controlador = false;
+            }
+
+            Console.WriteLine(Resultado(controlador));
+        }
+
+        public static string Resultado(bool controlador)
+        {
+            return (controlador)? "YOU WIN": "GAME OVER";
+        }
+
+    }
+}
